bf47551689f9304247cdde0dc5298c138654edd2
talitasv <talitasantos@visaojr.com.br>
2017-12-22 19:24:24 -0200

    cadastrar produtos com backend

--- cadastrarProduto.php
+++ cadastrarProduto.php
@@ -1,3 +1,28 @@
+<?php
+require_once 'assets/php/classes/classProduto.php';
+
+$prod = new Produto();
+
+if(isset($_POST['insert'])){
+	$prod->setNome($_POST['nome']);
+	$prod->setQuantidade($_POST['quantidade']);
+	$prod->setDescricao($_POST['descricao']);
+	$prod->setPrecoCusto($_POST['precoCusto']);
+	$prod->setPrecoVenda($_POST['precoVenda']);
+
+	if($prod->insert() == 1){
+		$result = "Produto inserido com sucesso!";
+	}else{
+		$error = "Erro ao inserir";
+	}
+}
+
+if(isset($_POST['cancel'])){
+	header("Location: cadastrarProduto.php");
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -9,8 +34,23 @@
 	<title>LaMantequilla</title>
 </head>
 <body>
-	<div class="col-md-4">
-		<h1>CADASTRAR PRODUTOS</h1>
+	<h1>CADASTRAR PRODUTOS</h1>
+	<?php
+	if (isset($result)) {
+		?>
+		<div class="alert alert-success">
+			<?php echo $result; ?>
+		</div>
+		<?php
+	}else if(isset($error)){
+		?>
+		<div class="alert alert-danger">
+			<?php echo $error; ?>
+		</div>
+		<?php
+	}
+	?>
+	<div class="col-md-6">
 		<form action="cadastrarProduto.php" method="post">
 			<div class="form-group">
 				<label>Nome</label>
@@ -18,7 +58,7 @@
 				<label>Quantidade</label>
 				<input type="number" name="quantidade" class="form-control">
 				<label>Descrição</label>
-				<textarea class="form-control"></textarea>
+				<textarea name="descricao" class="form-control"></textarea>
 				<label>Preço Custo</label>
 				<input type="text" name="precoCusto" class="form-control" onkeyup="moeda(this);">
 				<label>Preço Venda</label>
@@ -36,7 +76,7 @@
 <script src="js/bootstrap.min.js"></script>
 <script>
 	function moeda(z) {
-        v = z.value;
+		v = z.value;
         v = v.replace(/\D/g, "") // permite digitar apenas numero
         v = v.replace(/(\d{1})(\d{14})$/, "$1.$2") // coloca ponto antes dos ultimos digitos
         v = v.replace(/(\d{1})(\d{11})$/, "$1.$2") // coloca ponto antes dos ultimos 11 digitos
