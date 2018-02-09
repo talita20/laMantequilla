80ce4146e956e8e2d1ba3fadcd4c2165d8c988ab
talitasv <talitasantos@visaojr.com.br>
2018-02-09 20:59:28 -0200

    crud produtos

--- SistemaES/cadastrarProduto.php
+++ SistemaES/cadastrarProduto.php
@@ -1,71 +1,120 @@
 <?php
-	require_once 'header.php';
+require_once 'header.php';
+require_once '../assets/php/classes/classProduto.php';
+
+$prod = new Produto();
+
+if(isset($_POST['insert'])){
+  $prod->setNome($_POST['nome']);
+  $prod->setQuantidade($_POST['quantidade']);
+  $prod->setDescricao($_POST['descricao']);
+  $prod->setPrecoCusto($_POST['precoCusto']);
+  $prod->setPrecoVenda($_POST['precoVenda']);
+
+  if($prod->insert() == 1){
+    $result = "Produto inserido com sucesso!";
+  }else{
+    $error = "Erro ao inserir";
+  }
+}
+
+if(isset($_POST['cancel'])){
+  header("Location: cadastrarProduto.php");
+}
+
 ?>
-	 <div class="content-wrapper">
-		<div id="main" class="container-fluid">
-          <!-- general form elements disabled -->
-          <div class="box box-warning" style="border-color:#222D32; margin-top: 20px;">
-            <div class="box-header with-border">
-              <h3 class="box-title">Cadastrar Produto</h3>
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-              <form role="form">
-                <!-- text input -->
-                <div class="form-group">
-                  <label>Código</label>
-                  <input type="text" class="form-control" placeholder="Digite o código">
-                </div>
-                <div class="form-group">
-                  <label>Nome</label>
-                  <input type="text" class="form-control" placeholder="Digite o nome" >
-                </div>
-                <div class="form-group">
-                  <label>Quantidade</label>
-                  <input type="number" class="form-control" placeholder="Digite a quantidade" >
-                </div>
-                <div class="form-group">
-                  <label>Descrição</label>
-                  <textarea class="form-control" rows="3" placeholder="Descreva o produto"></textarea>
-                </div>
-                <label>Preço de Custo</label>
-                <div class="input-group">  
-                <span class="input-group-addon">R$</span>
-                <input type="text" class="form-control">
-              </div>
-              <label>Preço de Venda</label>
-                <div class="input-group">
-                <span class="input-group-addon">R$</span>
-                <input type="text" class="form-control">
-              </div>
-              </form>
-            </div>
-           <div class="box-footer">
-            <button type="submit" class="btn btn-success">Enviar</button>
+<div class="content-wrapper">
+  <?php
+  if (isset($result)) {
+    ?>
+    <div class="alert alert-success">
+      <?php echo $result; ?>
+    </div>
+    <?php
+  }else if(isset($error)){
+    ?>
+    <div class="alert alert-danger">
+      <?php echo $error; ?>
+    </div>
+    <?php
+  }
+  ?>
+  <div id="main" class="container-fluid">
+    <!-- general form elements disabled -->
+    <div class="box box-warning" style="border-color:#222D32; margin-top: 20px;">
+      <div class="box-header with-border">
+        <h3 class="box-title">Cadastrar Produto</h3>
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">
+        <form role="form" action="cadastrarProduto.php" method="post">
+          <!-- text input -->
+         <!--  <div class="form-group">
+            <label>Código</label>
+            <input type="text" nome="codigo" class="form-control" placeholder="Digite o código" required>
+          </div> -->
+          <div class="form-group">
+            <label>Nome</label>
+            <input type="text" name="nome" class="form-control" placeholder="Digite o nome" required>
+          </div>
+          <div class="form-group">
+            <label>Quantidade</label>
+            <input type="number" name="quantidade" class="form-control" placeholder="Digite a quantidade" required>
+          </div>
+          <div class="form-group">
+            <label>Descrição</label>
+            <textarea class="form-control" name="descricao" rows="3" placeholder="Descreva o produto"></textarea>
+          </div>
+          <label>Preço de Custo</label>
+          <div class="input-group">  
+            <span class="input-group-addon">R$</span>
+            <input type="text" name="precoCusto" class="form-control" onkeyup="moeda(this);">
+          </div>
+          <label>Preço de Venda</label>
+          <div class="input-group">
+            <span class="input-group-addon">R$</span>
+            <input type="text" name="precoVenda" class="form-control" onkeyup="moeda(this);">
+          </div>
+          <div class="box-footer">
+            <button type="submit" name="insert" class="btn btn-success">Enviar</button>
             <a href="./produtos.php">
-            <button type="submit" class="btn btn-danger">Cancelar</button>
-          </a>
+              <button type="submit" class="btn btn-danger">Cancelar</button>
+            </a>
           </div>
-            <!-- /.box-body -->
-          </div>
-         </div>
+        </form>
+      </div>
+      
+      <!-- /.box-body -->
+    </div>
+  </div>
 
 
-         </div>
+</div>
 
 <script type="application/javascript">
-    var active = document.getElementById("produtos");
-    active.classList.add("active");
+  var active = document.getElementById("produtos");
+  active.classList.add("active");
 </script>
 
 
 <script type="application/javascript">
-    var active = document.getElementById("produtoscadastrar");
-    active.classList.add("active");
+  var active = document.getElementById("produtoscadastrar");
+  active.classList.add("active");
+
+  function moeda(z) {
+        v = z.value;
+        v = v.replace(/\D/g, "") // permite digitar apenas numero
+        v = v.replace(/(\d{1})(\d{14})$/, "$1.$2") // coloca ponto antes dos ultimos digitos
+        v = v.replace(/(\d{1})(\d{11})$/, "$1.$2") // coloca ponto antes dos ultimos 11 digitos
+        v = v.replace(/(\d{1})(\d{8})$/, "$1.$2") // coloca ponto antes dos ultimos 8 digitos
+        v = v.replace(/(\d{1})(\d{5})$/, "$1.$2") // coloca ponto antes dos ultimos 5 digitos
+        v = v.replace(/(\d{1})(\d{1,2})$/, "$1,$2") // coloca virgula antes dos ultimos 2 digitos
+        z.value = v;
+    }
 </script>
 
 
 
 <?php
-    require_once 'footer.php';
+require_once 'footer.php';
 ?>
--- SistemaES/produtos.php
+++ SistemaES/produtos.php
@@ -1,113 +1,167 @@
 <?php 
 require_once 'header.php';
+require_once '../assets/php/classes/classProduto.php';
+
+$prod = new Produto();
+
+if(isset($_POST['edit'])){
+	$prod->setIdProduto($_POST['idProduto']);
+	$prod->setNome($_POST['nome']);
+	$prod->setQuantidade($_POST['quantidade']);
+	$prod->setDescricao($_POST['descricao']);
+	$prod->setPrecoCusto($_POST['precoCusto']);
+	$prod->setPrecoVenda($_POST['precoVenda']);
+
+
+	if($prod->edit() == 1){
+		$result = "Produto editado com sucesso!";
+	}else{
+		$error = "Erro ao editar";
+	}
+
+}
+
+if(isset($_POST['delete'])){
+	$prod->setIdProduto($_POST['idProduto']);
+
+	if($prod->delete() == 1){
+		$result = "Produto deletado com sucesso!";
+	}else{
+		$error = "Erro ao deletar";
+	}
+}
+
 ?>
-  <div class="content-wrapper">
-<div id="main" class="container-fluid">
- 
- 	<div id="top" class="row">
- 		<div class="col-md-12">
- 			<h1 align="center">Produtos</h1>
- 		</div>
-		<div class="col-sm-3">
-		</div>
-		<div class="col-sm-6">
-			
-			<div class="input-group h2">
-				<input name="data[search]" class="form-control" id="search" type="text" placeholder="Pesquisar Itens">
-				<span class="input-group-btn">
-					<button class="btn btn-primary" type="submit">
-						<span class="glyphicon glyphicon-search"></span>
-					</button>
-				</span>
+<div class="content-wrapper">
+	<div id="main" class="container-fluid">
+		<?php
+		if (isset($result)) {
+			?>
+			<div class="alert alert-success">
+				<?php echo $result; ?>
 			</div>
-			
-		</div>
-	</div> <!-- /#top -->
- 
- 
- 	<hr />
- 	<div id="list" class="row">
-	
-	<div class="table-responsive col-md-12">
-		<table class="table table-striped" cellspacing="0" cellpadding="0">
-			<thead>
-				<tr>
-					<th>Código</th>
-					<th>Nome</th>
-					<th>Descrição</th>
-					<th>Quantidade</th>
-					<th>Pço. Custo</th>
-					<th>Pço. Venda</th>
-					<th class="actions">Ações</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>01</td>
-					<td>Farinha</td>
-					<td>Integral</td>
-					<td>400</td>
-					<td>R$5,00</td>
-					<td>R$10,00</td>
-					<td class="actions">
-						<a class="btn btn-warning btn-xs" href="editarProduto.php">Editar</a>
-						<a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal">Excluir</a>
-					</td>
-				</tr>
-			
-			</tbody>
-		</table>
-	</div>
-	
-	</div> <!-- /#list -->
-	
-	<div id="bottom" class="row">
-		<div class="col-md-12">
-			<ul class="pagination">
-				<li class="disabled"><a>&lt; Anterior</a></li>
-				<li class="disabled"><a>1</a></li>
-				<li><a href="#">2</a></li>
-				<li><a href="#">3</a></li>
-				<li class="next"><a href="#" rel="next">Próximo &gt;</a></li>
-			</ul><!-- /.pagination -->
-		</div>
-	</div> <!-- /#bottom -->
- </div> <!-- /#main -->
+			<?php
+		}else if(isset($error)){
+			?>
+			<div class="alert alert-danger">
+				<?php echo $error; ?>
+			</div>
+			<?php
+		}
+		?>
+		<div id="top" class="row">
+			<div class="col-md-12">
+				<h1 align="center">Produtos</h1>
+			</div>
+			<div class="col-sm-3">
+			</div>
+			<div class="col-sm-6">
 
-<!-- Modal -->
-<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="modalLabel">Excluir Item</h4>
-      </div>
-      <div class="modal-body">
-        Deseja realmente excluir este item?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Sim</button>
-	<button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
+				<div class="input-group h2">
+					<input name="data[search]" class="form-control" id="search" type="text" placeholder="Pesquisar Itens">
+					<span class="input-group-btn">
+						<button class="btn btn-primary" type="submit">
+							<span class="glyphicon glyphicon-search"></span>
+						</button>
+					</span>
+				</div>
+
+			</div>
+		</div> <!-- /#top -->
 
 
-<script type="application/javascript">
-    var active = document.getElementById("produtos");
-    active.classList.add("active");
-</script>
+		<hr />
+		<div id="list" class="row">
+
+			<div class="table-responsive col-md-12">
+				<table class="table table-striped" cellspacing="0" cellpadding="0">
+					<thead>
+						<tr>
+							<!-- <th>Código</th> -->
+							<th>Nome</th>
+							<th>Descrição</th>
+							<th>Quantidade</th>
+							<th>Pço. Custo</th>
+							<th>Pço. Venda</th>
+							<th class="actions">Ações</th>
+						</tr>
+						<?php 
+						$stmt = $prod->index();
+						while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+							?>
+						</thead>
+						<tbody>
+							<tr>
+								<!-- <td>01</td> -->
+								<td><?php echo $row->nome ?></td>
+								<td><?php echo $row->descricao ?></td>
+								<td><?php echo $row->quantidade ?></td>
+								<td><?php echo $row->precoCusto ?></td>
+								<td><?php echo $row->precoVenda ?></td>
+								<td class="actions">
+									<a class="btn btn-warning btn-xs" href="editarProduto.php">Editar</a>
+									<a class="btn btn-danger btn-xs"  href="#" data-toggle="modal" data-target="#delete-modal<?php echo $row->idProduto ?>">Excluir</a>
+								</td>
+							</tr>
+							<?php } ?>
+						</tbody>
+					</table>
+				</div>
+
+			</div> <!-- /#list -->
+
+			<div id="bottom" class="row">
+				<div class="col-md-12">
+					<ul class="pagination">
+						<li class="disabled"><a>&lt; Anterior</a></li>
+						<li class="disabled"><a>1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li class="next"><a href="#" rel="next">Próximo &gt;</a></li>
+					</ul><!-- /.pagination -->
+				</div>
+			</div> <!-- /#bottom -->
+		</div> <!-- /#main -->
+		<?php 
+		$stmt = $prod->index();
+		while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+			?>
+			<form action="produtos.php" method="post">
+				<!-- Modal -->
+				<div class="modal fade" id="delete-modal<?php echo $row->idProduto ?>" tabindex="-1" role="dialog" aria-labelledby="modalLabel">
+					<div class="modal-dialog" role="document">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
+								<h4 class="modal-title" id="modalLabel">Excluir Item</h4>
+							</div>
+							<div class="modal-body">
+								Deseja realmente excluir o item <?php echo $row->nome ?>?
+							</div>
+							<div class="modal-footer">
+								<input type="hidden" name="idProduto" value="<?php echo $row->idProduto ?>">
+								<button type="submit" name="delete" class="btn btn-primary">Sim</button>
+								<button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
+
+			<script type="application/javascript">
+				var active = document.getElementById("produtos");
+				active.classList.add("active");
+			</script>
 
 
-<script type="application/javascript">
-    var active = document.getElementById("produtosvisualizar");
-    active.classList.add("active");
-</script>
+			<script type="application/javascript">
+				var active = document.getElementById("produtosvisualizar");
+				active.classList.add("active");
+			</script>
 
 
 
-<?php
-require_once 'footer.php';
-?>
+			<?php
+			require_once 'footer.php';
+			?>
